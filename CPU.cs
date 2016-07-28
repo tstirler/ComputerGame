5d770b98f02d9b8d84b1a88770573bbc472bbf0a
tstirler <torbjorn@stirler.no>
2016-07-28 18:12:13 +0200

    Initial

---
+++ CPU.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace ComputerGame
+{
+    class CPU
+    {
+        private byte _dataBus;
+        private ushort _addressBus;
+
+        public byte DataBus { get { return _dataBus; } }
+        public ushort AdressBus { get { return _addressBus; } }
+
+        public CPU()
+        {
+
+        }
+
+        public void Update()
+        {
+
+        }
+    }
+}
